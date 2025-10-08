8ba34ca6703eb0358eb2c645db1d111cd3c74a21
Gustavo Lopes <glopesaraujo6@gmail.com>
2025-10-07 22:26:32 -0300

    1

--- mysite/polls/static/js/script.js
+++ mysite/polls/static/js/script.js
@@ -1,11 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
-  // pagination: { el: ".swiper-pagination", clickable: true }, // se quiser bolinhas
-  breakpoints: {
-    0:    { slidesPerView: 1 },
-    576:  { slidesPerView: 2 },
-    992:  { slidesPerView: 3 }
-  }
-});
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
