23dbad1e1982c90324af146de39ce525667be37d
Gustavo Lopes <glopesaraujo6@gmail.com>
2025-10-29 22:47:26 -0300

    t

--- mysite/polls/static/js/script.js
+++ mysite/polls/static/js/script.js
@@ -1,3 +1,50 @@
-import { Ripple, initMDB } from "mdb-ui-kit";
+document.addEventListener('DOMContentLoaded', function() {
+    // Services Swiper
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            992: {
+                slidesPerView: 4,
+            },
+            1200: {
+                slidesPerView: 5,
+            }
+        },
+        loop: false,
+    });
 
-initMDB({ Ripple });
+    // Team Swiper
+    var teamSwiper = new Swiper(".teamSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
+    });
+});
